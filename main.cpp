760e483c1cce945da18d1cad90691b464deec9a9
mikke <mikael.lauvlid@hotmail.no>
2026-08-20 19:46:22 +0200

    Basic BFS implemented (not with support for edge cases yet)

--- main.cpp
+++ main.cpp
@@ -7,12 +7,20 @@
 #include <string>
 #include <queue>
 #include <array>
+#include <algorithm>
 
 struct Position {
-    int x, y;
+    int x{}, y{};
+
+    bool operator==(const Position& other) const {
+        return x == other.x && y == other.y;
+    }
+    bool operator!=(const Position& other) const {
+        return x != other.x || y != other.y;
+    }
 };
 
-const std::vector<std::string> maze {
+std::vector<std::string> maze {
     "+----------+",
     "|....|.....|",
     "|....|.....|",
@@ -35,9 +43,15 @@ std::vector<Position> getAvailableNeighbours(const Position pos) {
     std::vector<Position> neighbours;
 
     for (int i = 0; i < xOffsets.size(); i++) {
-        char c = maze[pos.y + yOffsets[i]][pos.x + xOffsets[i]];
+        const int yPos = pos.y + yOffsets[i], xPos = pos.x + xOffsets[i];
+
+        if (yPos >= maze.size() || yPos < 0 ||  xPos >= maze[0].size() || xPos < 0) {
+            continue;
+        }
+
+        const char c = maze[yPos][xPos];
         if (c == '.' || c == 'P') {
-            neighbours.push_back(pos);
+            neighbours.push_back({.x = xPos, .y= yPos});
         }
     }
 
@@ -47,24 +61,44 @@ std::vector<Position> getAvailableNeighbours(const Position pos) {
 void findShortestPath(const Position start, const Position end) {
     std::queue<Position> toVisitQueue;
     std::vector<Position> visitedPositions;
-    std::vector<std::vector<Position>> previous; // What position did I come from when I reached (x,y)?
+    std::vector<std::vector<Position>> previous(maze.size(), std::vector<Position>(maze[0].size())); // What position did I come from when I reached (x,y)?
 
     toVisitQueue.push(start);
 
     while (!toVisitQueue.empty()) {
         const Position current = toVisitQueue.front();
+        toVisitQueue.pop();
+
         std::vector<Position> neighbours = getAvailableNeighbours(current);
 
         for (const Position& neighbour : neighbours) {
-            toVisitQueue.push(neighbour);
+            if (std::find(visitedPositions.begin(), visitedPositions.end(), neighbour) == visitedPositions.end()) {
+                toVisitQueue.push(neighbour);
+                visitedPositions.push_back(neighbour); // Avoid getting queued again later
+                previous[neighbour.y][neighbour.x] = current;
+            }
         }
     }
 
+    Position currentPos = end;
+    int totalSteps = 0;
+
+    while (currentPos != start) {
+        ++totalSteps;
+        maze[currentPos.y][currentPos.x] = '\\';
+        currentPos = previous[currentPos.y][currentPos.x];
+    }
+
+    maze[start.y][start.x] = 'X'; maze[end.y][end.x] = 'X';
+
+    std::cout << totalSteps << std::endl;
 }
 
 
 
 int main() {
+
+    findShortestPath({.x = 4,.y = 5},{.x = 6, .y = 5});
     printMaze();
 
     return 0;
